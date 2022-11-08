afc5f1244150b4607796af1507f5b3c2f06f4892
takumi <t.souma.sys22@morijyobi.ac.jp>
2022-11-08 12:43:13 +0900

    Main.java C³

--- Main.java
+++ Main.java
@@ -1,14 +1,23 @@
 import java.util.Scanner;
 
-public class Main {
+public class Ensyu2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("霄ｫ髟ｷ(m):");
+        System.out.print("一人目の身長(m):");
         double heigth = sc.nextDouble();
-        System.out.print("菴馴㍾(kg):");
+        System.out.print("一人目の体重(kg):");
         double weight = sc.nextDouble();
+        
+        System.out.print("二人目の身長(m):");
+        double heigth2 = sc.nextDouble();
+        System.out.print("二人目の体重(kg):");
+        double weight2 = sc.nextDouble();
         double a = heigth * heigth;
+        double a2 = heigth2 * heigth2;
         double BMI = weight / a;
-        System.out.println("あなたのBMI値は"+((Math.floor(BMI * 100)) / 100)+"です。");
+        double BMI2 = weight / a2;
+
+        System.out.println((Math.floor(BMI * 100)) / 100);
+        System.out.println((Math.floor(BMI2 * 100)) / 100);
     }
 }
